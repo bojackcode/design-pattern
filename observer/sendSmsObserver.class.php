b68316682525b6edf714dfe0037e952429290baf
liangbopirates <liangbopirates@163.com>
2015-03-25 10:03:02 +0800

    del

--- observer/sendSmsObserver.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-class sendSmsObserver implements Observer{
-    public function update($subject){
-        $order=$subject->getOrder();
-        //vip的短信可能和普通用户的不一样
-        echo "nickName=$order->nickName:orderNumber is $order->id take;\n";
-    }
-}
